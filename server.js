c555baf8ad379d1cd4caebf1a7aed498183915e3
sspohC <sspohC@gmail.com>
2016-03-06 16:47:12 -0800

    Fixed json merge

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/wellNumbers', function(req, res, next) {
     for (var i = 4; i < 55000; i += 11) {
       var wellJson = {}
       wellJson.num = $('table td').eq([i]).text();
-      wellNum.push(wellJson);
+      wellNum.push(wellJson.num);
       console.log([i]);
       // console.log(wellJson)
     };
@@ -35,7 +35,7 @@ app.get('/wellNumbers2', function(req, res, next) {
     for (var i = 4; i < 21000; i += 11) {
       var wellJson = {};
       wellJson.num = $('table td').eq([i]).text();
-      wellNum.push(wellJson);
+      wellNum.push(wellJson.num);
       console.log([i])
     };
     fs.writeFile('claiborne2.json', JSON.stringify(wellNum, null, 4), function(err) {
@@ -61,4 +61,103 @@ app.get('/combineWells', function(req, res, next) {
   })
 
 })
+app.get('/wellsNoKey', function(req, res, next) {
+  var wellNum;
+  var wellNum2;
+  fs.readFile('claiborne1.json', 'utf-8', function(err, data) {
+    wellNum = JSON.parse(data);
+    console.log(wellNum)
+    fs.readFile('claiborne2.json', 'utf-8', function(err, data) {
+      wellNum2 = JSON.parse(data);
+      var newArray = wellNum.concat(wellNum2)
+      console.log(newArray.length)
+      // console.log(wellNum)
+      fs.writeFile('claiborne.json', JSON.stringify(newArray, null, 4), function(err) {
+        if (err) throw err;
+      })
+    })
+  })
+
+})
+app.get('/scrapeClaiborne', function(req, res, next) {
+  fs.readFile('claiborne.json', 'utf-8', function(err, data) {
+    for (var key in data) {
+      function myLoop() {
+        setTimeout(function() {
+          console.log(key)
+          if (key < 50){
+            myLoop()
+          }
+        }, 4000)
+      }
+              myLoop();
+    }
+  })
+
+})
+
+// app.get('/testing', function(req, res, next) {
+//   var wellArray = [];
+//   fs.readFile('claiborne.json', 'utf-8', function(err, data) {
+//     wellArray = JSON.parse(data);
+//     //console.log(wellArray);
+//     // for (var i = 0; i < wellArray.length; i++) {
+//     //   console.log([i])
+//     // }
+//     var len = wellArray.length;
+//     var i = 0;
+//     // console.log(wellArray[0]);
+//     // for (var i = 0; i < len; i++) {
+//     //   console.log(wellArray[i])
+//     // }
+//
+//     var i = 1
+//     var wellArray = [];
+//     function myLoop() {
+//       setTimeout(function() {
+//         url = 'http://localhost:8080/well' +i+ '.html';
+//         request(url, function(err, res, html) {
+//
+//           var json = {};
+//
+//           console.log(wellArray)
+//             var $ = cheerio.load(html);
+//           json.serialNum = $('table td').eq(2).text();
+//           json.orgId = $('table td').eq(3).text();
+//           wellArray.push(json)
+//           // console.log(wellArray)
+//         })
+//         i++
+//
+//         if (i < len){myLoop()}
+//
+//       }, 2000)
+//
+//     }
+//
+//       myLoop()
+app.get('/lolzor', function(req, res, next) {
+  var i = 1
+  var wellArray = [];
+  function myLoop() {
+    setTimeout(function() {
+      url = 'http://localhost:8080/well' +i+ '.html';
+      request(url, function(err, res, html) {
+        var json = {};
+        //console.log(wellArray)
+        var $ = cheerio.load(html);
+        json.serialNum = $('table td').eq(2).text();
+        if (json.serialNum === '') return
+        json.orgId = $('table td').eq(3).text();
+        wellArray.push(json)
+        console.log(wellArray)
+        fs.writeFile('output.json', JSON.stringify(wellArray, null, 4), function(err) {})
+      })
+      i++
+      if (i < 4){myLoop()}
+    }, 2000)
+  }
+    myLoop()
+});
+
 app.listen(9001);
